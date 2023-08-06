288145bc495751d152d274f4884351f4247f2489
desktop <ryo.kokai.g@gmail.com>
2023-08-06 10:08:24 +0900

    パンくずリスト作成予定を追記

--- src/page/request.vue
+++ src/page/request.vue
@@ -1,7 +1,7 @@
 
 <template>
   <v-layout>
-    <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+    <v-app-bar class="main_bar" color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
       <template v-slot:image>
         <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
       </template>
@@ -22,7 +22,9 @@
         </v-btns>
       </template>
     </v-app-bar>
+    
     <v-main>
+      パンくずリスト
       <div class="md">
         <!-- 本番用書き換え -->
         お問い合わせ
@@ -56,6 +58,5 @@ export default {
     }
   },
 }
-
 
 </script>
