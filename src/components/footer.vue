c88d18f51a2d7a9d75ffd773cbc3c869d143f076
desktop <ryo.kokai.g@gmail.com>
2023-08-12 18:10:20 +0900

    将来的にフッター問題(横幅が画面全体にならない)が解決したとき用にフッターコンポーネントを作成

---
+++ src/components/footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-footer dark color="teal-lighten-2" class="com_fotter" height="50px" >
+    <div class="flex-grow-1"></div>
+    <div>&copy; 2023 - {{ new Date().getFullYear() }} dbd-fishのブログ</div>
+  </v-footer>
+</template>
+
+<style></style>
