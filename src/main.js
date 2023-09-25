2664705dd3bf3a28ef3edc4ea84f7cf05daf88d7
desktop <ryo.kokai.g@gmail.com>
2023-09-25 21:48:18 +0900

    styleLintの自動修正を反映。手修正が残ってる。

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import App from "@/App.vue";
 import router from "@/router";
 import vuetify from "./vuetify";
 
-import VueAdsense from "vue-adsense";
-
 const pinia = createPinia();
 
 const app = createApp(App);
@@ -15,16 +13,6 @@ app.use(router);
 app.use(vuetify);
 app.use(pinia);
 
-app.component("VueAdsense", VueAdsense);
-
 app.mount("#app");
 
-const DEFAULT_TITLE = "Some Default Title";
-
-router.afterEach((to) => {
-  // Use next tick to handle router history correctly
-  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
-  Vue.nextTick(() => {
-    document.title = to.meta.title || DEFAULT_TITLE;
-  });
-});
+router.afterEach(() => {});
